4a59156745764b23ed29b000d66c7dee240342c7
Rika4698 <rikasharmin181@gmail.com>
2025-07-28 01:06:45 +0600

    update

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,71 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {NextFunction, Request, Response} from "express";
+import httpStatus from "http-status-codes";
+import { UserServices } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import { JwtPayload, verify } from "jsonwebtoken";
+import { verifyToken } from "../../utils/jwt";
+import { envVars } from "../../config/env";
+
+
+const createUser = catchAsync(async(req:Request, res:Response, next:NextFunction)=>{
+    const user = await UserServices.createUser(req.body)
+    // res.status(httpStatus.CREATED).json({
+    //     message:"User Created Successfully",
+    //     user
+    // })
+
+    sendResponse(res, {
+        success:true,
+        statusCode:httpStatus.CREATED,
+        message:"User Created Successfully",
+        data:user,
+    })
+})
+
+
+const updateUser = catchAsync(async(req:Request, res:Response, next:NextFunction)=>{
+    const userId = req.params.id;
+    const verifiedToken = req.user;
+    const payload = req.body;
+    const user = await UserServices.updateUser(userId, payload, verifiedToken)
+    // res.status(httpStatus.CREATED).json({
+    //     message:"User Created Successfully",
+    //     user
+    // })
+
+    sendResponse(res, {
+        success:true,
+        statusCode:httpStatus.CREATED,
+        message:"User Updated Successfully",
+        data:user,
+    })
+})
+
+
+
+const getAllUsers = catchAsync(async(req:Request, res:Response, next:NextFunction) =>{
+    const result = await UserServices.getAllUsers();
+    // res.status(httpStatus.OK).json({
+    //     success:true,
+    //     message:"All Users Retrieved Successfully",
+    //     data:users
+    // })
+
+    sendResponse(res, {
+        success:true,
+        statusCode:httpStatus.CREATED,
+        message:"All Users Retrieved Successfully",
+        data:result.data,
+        meta:result.meta
+    })
+    
+})
+
+export const UserControllers = {
+    createUser,
+    getAllUsers,
+    updateUser
+}
